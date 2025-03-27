8c6decbc53bcd2b78d12a280677b826f261192b8
padmaja48 <147294816+padmaja48@users.noreply.github.com>
2025-03-27 14:37:30 +0530

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -6,7 +6,7 @@ const Chatbot = () => {
   const [response, setResponse] = useState("");
 
   const sendMessage = async () => {
-    const res = await axios.post("http://localhost:8000/chat", { message: input });
+    const res = await axios.post("https://leo-ayud.onrender.com/chat", { message: input });
     setResponse(res.data.response);
   };
 
